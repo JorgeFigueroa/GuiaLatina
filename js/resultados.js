0b85ebfac12abf66067c76d91bb73a80cddd8f96
David Soto <sotod331@gmail.com>
2018-04-04 09:26:28 -0430

    Update 04/04/2018

--- js/resultados.js
+++ js/resultados.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	$('#rootwizard3').bootstrapWizard();
 	$('#rootwizard4').bootstrapWizard();
 
-	$('#hasDropDown').click(function(){
+	$('#hasDropDown, #hasDropDownPromo, #hasDropDownEvento').click(function(){
 		$('#dropDownItem').slideToggle('slow');
 	});
 
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		$('#dropDownItem').slideUp('slow');
 	});
 
-	$('#hasDropDown2').click(function(){
+	$('#hasDropDown2, #hasDropDownPromo2, #hasDropDownEvento2').click(function(){
 		$('#dropDownItem2').slideToggle('slow');
 	});
 	
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		$('#dropDownItem2').slideUp('slow');
 	});
 
-	$('#hasDropDown3').click(function(){
+	$('#hasDropDown3, #hasDropDownPromo3, #hasDropDownEvento3').click(function(){
 		$('#dropDownItem3').slideToggle('slow');
 	});
 	
@@ -29,12 +29,22 @@ $(document).ready(function() {
 		$('#dropDownItem3').slideUp('slow');
 	});
 
-	$('#hasDropDown4').click(function(){
+	$('#hasDropDown4, #hasDropDownPromo4, #hasDropDownEvento4').click(function(){
 		$('#dropDownItem4').slideToggle('slow');
 	});
 	
 	$('#closeItemBtn4').click(function(){
 		$('#dropDownItem4').slideUp('slow');
+	});
+
+	// Menu Sub-Categorias
+
+	$('#btnCategorias').click(function(){
+		$('#categoriasMenu').css('margin-left', '0');
+	});
+
+	$('#closeCatBtn').click(function(){
+		$('#categoriasMenu').css('margin-left', '-80%');
 	});
 
 
