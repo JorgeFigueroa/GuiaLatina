d98187c68d994870009ba5fef17ed88114a316c2
David Soto <30774739+sotod331@users.noreply.github.com>
2018-05-06 11:41:29 -0430

    Update 06-05-2018

--- js/resultados.js
+++ js/resultados.js
@@ -35,9 +35,6 @@ $(document).ready(function() {
 	});
 
 
-
-
-
 	$('#hasDropDown2').click(function(){
 		$('#promoEvento2').addClass('d-none');
 		$('#menuDiario2').removeClass('d-none');
@@ -152,6 +149,80 @@ $(document).ready(function() {
 	});
 
 
+
+
+	$('#hasDropDown5').click(function(){
+		$('#promoEvento5').addClass('d-none');
+		$('#menuDiario5').removeClass('d-none');
+		$('#dropDownItem5').slideToggle('slow');
+		$('#menuDiarioBtn5').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#promoEventoBtn5').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+
+	$('#hasDropDownPromo5, #hasDropDownEvento5').click(function(){
+		$('#menuDiario5').addClass('d-none');
+		$('#promoEvento5').removeClass('d-none');
+		$('#promoEventoBtn5').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#menuDiarioBtn5').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#dropDownItem5').slideDown('slow');
+	});
+
+	$('#promoEventoBtn5').click(function(){
+		$('#menuDiario5').addClass('d-none');
+		$('#promoEvento5').removeClass('d-none');
+		$('#promoEventoBtn5').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#menuDiarioBtn5').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+
+	$('#menuDiarioBtn5').click(function(){
+		$('#menuDiario5').removeClass('d-none');
+		$('#promoEvento5').addClass('d-none');
+		$('#menuDiarioBtn5').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#promoEventoBtn5').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+	
+	$('#closeItemBtn5').click(function(){
+		$('#dropDownItem5').slideUp('slow');
+	});
+
+
+
+
+
+
+	$('#hasDropDown6').click(function(){
+		$('#promoEvento6').addClass('d-none');
+		$('#menuDiario6').removeClass('d-none');
+		$('#dropDownItem6').slideToggle('slow');
+		$('#menuDiarioBtn6').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#promoEventoBtn6').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+
+	$('#hasDropDownPromo6, #hasDropDownEvento6').click(function(){
+		$('#menuDiario6').addClass('d-none');
+		$('#promoEvento6').removeClass('d-none');
+		$('#promoEventoBtn6').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#menuDiarioBtn6').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#dropDownItem6').slideDown('slow');
+	});
+
+	$('#promoEventoBtn6').click(function(){
+		$('#menuDiario6').addClass('d-none');
+		$('#promoEvento6').removeClass('d-none');
+		$('#promoEventoBtn6').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#menuDiarioBtn6').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+
+	$('#menuDiarioBtn6').click(function(){
+		$('#menuDiario6').removeClass('d-none');
+		$('#promoEvento6').addClass('d-none');
+		$('#menuDiarioBtn6').addClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+		$('#promoEventoBtn6').removeClass('border-top-0 border-left-0 border-right-0 border-bottom border-primary');
+	});
+	
+	$('#closeItemBtn6').click(function(){
+		$('#dropDownItem6').slideUp('slow');
+	});
 
 	
 
